6eb1ab0121f2529fda526809e53bc848b97fd4cd
Joseph Voss <jvoss@josephvoss.com>
2025-05-11 20:31:30 -0500

    Remove one KV usage, return status text

--- static/status.js
+++ static/status.js
@@ -1,37 +1,16 @@
 function checkStatus() {
   fetch("/check_status").then( (resp) => {
     if (!resp.ok) {
-      // login visible
-      // status_text hidden
-      // TODO we should return err. How to handle first thing?
-      // Assumed only err would be 404, should check status code
       return
     } else {
-      console.log("returning json")
-      return resp.json()
+      return resp.text()
     }
   }).then ((body) => {
     if (!body) {
       console.log("invalid body")
       return
     }
-    if (body.text) {
-      // update text
-      console.log("status text set")
-      document.getElementById("status_text").innerHTML = body.text;
-    }
-    if (body.retry) {
-      // set login and status visible
-      console.log("retry set")
-      document.getElementById("login_link").style.display = 'block';
-      document.getElementById("status_text").style.display = 'block';
-    }
-    if (body.finished) {
-      // goodbye
-      document.getElementById("login_link").style.display = 'hidden';
-      document.getElementById("status_text").style.display = 'block';
-      document.getElementById("status_text").innerHTML = 'Complete. Goodbye';
-    }
+    document.getElementById("status_text").innerHTML = body;
   })
 }
 
@@ -39,6 +18,7 @@ function checkStatus() {
 var sleep = duration => new Promise(resolve => setTimeout(resolve, duration))
 var poll = (promiseFn, duration) => promiseFn().then(
   sleep(duration).then(() => poll(promiseFn, duration)))
+checkStatus()
 // Run checkStatus every 5 sec
 poll( () => new Promise(
   () => {
